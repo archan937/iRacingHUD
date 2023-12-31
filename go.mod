4425fe86cbf91d717b5ed1814b43700b3fde2c56
Paul Engel <pm_engel@icloud.com>
2023-12-31 19:08:59 +0100

    Use iRacingSDK package

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module iRaceHUD
 
 go 1.21.5
+
+require github.com/quimcalpe/iracing-sdk v0.0.0-20220202205857-747744f2c906
+
+require (
+	github.com/hidez8891/shm v0.0.0-20200313135933-0ec4df5f28c7 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	irsdk "github.com/quimcalpe/iracing-sdk"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var sdk irsdk.IRSDK
+	sdk = irsdk.Init(nil)
+	defer sdk.Close()
+
+	for {
+		sdk.WaitForData(100 * time.Millisecond)
+		gear, _ := sdk.GetVar("CarIdxGear")
+		speed, _ := sdk.GetVar("Speed")
+		fmt.Printf("Gear: %s Speed: %s\n", gear, speed)
+	}
 }
